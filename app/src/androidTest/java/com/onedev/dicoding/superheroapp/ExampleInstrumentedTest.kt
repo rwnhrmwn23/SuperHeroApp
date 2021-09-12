e079607b6c53bc176608ebe6ba26d9b29167719a
yogawicak <rwnhermawan@gmail.com>
2021-09-12 18:54:16 +0700

    add test

--- app/src/androidTest/java/com/onedev/dicoding/superheroapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/onedev/dicoding/superheroapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.onedev.dicoding.superheroapp.favorite", appContext.packageName)
+        assertEquals("com.onedev.dicoding.superheroapp.app", appContext.packageName)
     }
 }
